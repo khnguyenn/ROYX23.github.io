3064a70bc773a4a15fdac34c30439f71e68c0f3a
ROYX23 <139224098+ROYX23@users.noreply.github.com>
2023-07-11 21:51:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -135,19 +135,27 @@ var swiper = new Swiper(".bg-slider-thumbs", {
   window.addEventListener('scroll', updateHeaderColor);
 
 
-// prevent f12 & inspect source
- $(document).keydown(function (event) {
-                if (event.keyCode == 123) { // Prevent F12
-                    return false;
-                } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
-                    return false;
-                }
-            });
-            $(document).on("contextmenu", function (e) {
-                e.preventDefault();
-            });
-
-// set debugger
-            setInterval(function () {
-                debugger;
-            }, 50);
+document.addEventListener('contextmenu', event => event.preventDefault());
+ 
+    document.onkeydown = function (e) {
+ 
+        // disable F12 key
+        if(e.keyCode == 123) {
+            return false;
+        }
+ 
+        // disable I key
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+            return false;
+        }
+ 
+        // disable J key
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+            return false;
+        }
+ 
+        // disable U key
+        if(e.ctrlKey && e.keyCode == 85) {
+            return false;
+        }
+    }
